b3d0fc7939172a40a724147e3745481aba93632e
LuizGiserman <luizgiserman@poli.ufrj.br>
2020-11-08 20:35:32 -0300

    Improving Markov

--- babel/babel.py
+++ babel/babel.py
@@ -1,2 +1,11 @@
+from markov import Markov
 
+# speed = 0
 
+map_tempo = ['half', 'full', 'two_full', 'four_full']
+train = '00123311101'
+
+tempo = Markov(4, train, map_tempo)
+print (tempo.dFrame)
+predictions = tempo.predict('half', 5)
+print(predictions)
--- babel/markov.py
+++ babel/markov.py
@@ -3,10 +3,12 @@ import pandas as pd
 
 class Markov:
 
-    def __init__ (self, n, data):
+    def __init__ (self, n, data, mapping=None):
         self.n = n
         self.data = data
-        self.probabilities = ''
+        self.probabilities = None
+        self.dFrame = None
+        self.mapping = mapping
         self.train()
 
     def train(self):
@@ -22,8 +24,27 @@ class Markov:
         sum_of_rows = aux.sum(axis=1)
         self.probabilities = aux / sum_of_rows[:, np.newaxis] 
 
+        #building the dataframe
+        try:
+            self.dFrame = pd.DataFrame(self.probabilities, index=self.mapping, columns=self.mapping)
+        except:
+            self.dFrame = pd.DataFrame(self.probabilities, index=range(self.n), columns=range(self.n))
+        return
+
+    def choose_next(self, base_name):
+        return self.dFrame.idxmax(axis=1)[base_name]
 
 
-teste = Markov(2, '001010111')
-print(teste.probabilities)
+    def predict(self, base_name, num_predictions):
+        result_list = [base_name]
+        aux = base_name
+        for _ in range(num_predictions):
+            aux = self.choose_next(aux)
+            result_list.append(aux)
+        return result_list
 
+
+
+# teste = Markov(2, '001010111')
+# print(teste.probabilities)
+# print (teste.dFrame)
