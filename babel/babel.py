3d18bd8e5fa23671fe77a027c6026f45889e8a51
Pedro <pedromxavier@poli.ufrj.br>
2020-11-09 01:28:57 -0300

    Update babel.py __doc__

--- babel/babel.py
+++ babel/babel.py
@@ -1,6 +1,15 @@
-from markov import Markov
+""" 0 : (1/2)^0 = 1
+    1 : (1/2)^1 = 1/2
+    2 : (1/2)^2 = 1/4
+    3 : (1/2)^3 = 1/8
+    4 : (1/2)^4 = 1/16
+"""
+from .markov import Markov
 
 # speed = 0
+##
+#
+#
 
 map_tempo = {0:'half', 1:'full', 2:'two_full', 3:'four_full'}
 train = '00123311101'
