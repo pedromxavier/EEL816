403f7c8f6710a7689cf30657930ba37bc7615b57
Pedro <pedromxavier@poli.ufrj.br>
2020-11-09 02:41:52 -0300

    Create Markos

--- babel/babel.py
+++ babel/babel.py
@@ -3,19 +3,34 @@
     2 : (1/2)^2 = 1/4
     3 : (1/2)^3 = 1/8
     4 : (1/2)^4 = 1/16
+    5 : (1/2)^5 = 1/32
+    6 : (1/2)^6 = 1/64
+
+    4 : 4 notas de referência
+    4 : nota de referência é a 1/4 (índice 2)
+
 """
-from .markov import Markov
+from fractions import Fraction
+from markos import Markos, TimeMarkos
 
-# speed = 0
-##
-#
-#
+class Babel:
 
-map_tempo = {0:'half', 1:'full', 2:'two_full', 3:'four_full'}
-train = '00123311101'
+    def __init__(self, **kwargs):
+        ...
 
-tempo = Markov(4, train)
-predictions = tempo.predict(0, 5)
-print(predictions)
-translation = [map_tempo[key] for key in predictions]
-print (translation)
+C = (4, 4)
+
+data = '0 0 1233 11 11 11'
+
+tempo = TimeMarkos(C)
+tempo.seed(0)
+tempo.train([x for x in data if x != ' '])
+k = 0
+for t in tempo:
+    if k >= 12: break
+    if t is None:
+        k += 1
+        print(' ', end='')
+    else:
+        print(t, end='')
+print()
---
+++ babel/markos.py
@@ -0,0 +1,72 @@
+from fractions import Fraction
+import numpy as np
+
+class Markos:
+
+    def __init__ (self, n):
+        self.n = n
+        self.r = np.arange(0, n, dtype=int)
+        self._p = np.zeros(shape=(self.n, self.n))
+        self._p_normal = None
+        self._p_dirty = True
+        
+        self.prev = None
+
+    @property
+    def p(self):
+        if self._p_dirty:
+            self._p_normal = self._p / (np.sum(self._p, axis=1)[:, np.newaxis] + (self._p == 0))
+            self._p_dirty = False
+        return self._p_normal
+
+    def seed(self, x: int):
+        self.prev = x
+
+    def train(self, data):
+        for (current, next) in zip(data, data[1:]): 
+            self._p[int(current)][int(next)] += 1
+        else:
+            self._p_dirty = True
+
+    def __next__(self):
+        self.prev = np.random.choice(self.r, p=self.p[self.prev])
+        return self.prev
+
+    def __iter__(self):
+        while True:
+            yield self.__next__()
+
+class TimeMarkos(Markos):
+
+    FZERO = Fraction(0)
+
+    def __init__(self, C: tuple):
+        self.s = self.FZERO
+        self.L = Fraction(C[0], C[1])
+        Markos.__init__(self, 7)
+
+    def __iter__(self):
+        while True:
+            t = self.__next__()
+            r = Fraction(1, pow(2, t))
+            while True:
+                if r + self.s == self.L: ## fecha o compasso
+                    self.s = self.FZERO
+                    yield t
+                    yield None
+                    break
+                elif r + self.s < self.L:
+                    self.s = self.s + r
+                    yield t
+                    break
+                else:
+                    t += 1
+                    r = Fraction(1, pow(2, t))
+                    continue
+
+
+class ChordMarkos(Markos):
+    ...
+
+class NoteMarkos(Markos):
+    ...
