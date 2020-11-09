a3e719680a934ac831c1a1f666d6f8754f7173fa
LuizGiserman <luizgiserman@poli.ufrj.br>
2020-11-09 01:06:51 -0300

    Switched to np

--- babel/babel.py
+++ babel/babel.py
@@ -2,10 +2,11 @@ from markov import Markov
 
 # speed = 0
 
-map_tempo = ['half', 'full', 'two_full', 'four_full']
+map_tempo = {0:'half', 1:'full', 2:'two_full', 3:'four_full'}
 train = '00123311101'
 
-tempo = Markov(4, train, map_tempo)
-print (tempo.dFrame)
-predictions = tempo.predict('half', 5)
+tempo = Markov(4, train)
+predictions = tempo.predict(0, 5)
 print(predictions)
+translation = [map_tempo[key] for key in predictions]
+print (translation)
